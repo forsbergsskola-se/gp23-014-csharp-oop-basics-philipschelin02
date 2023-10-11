c22e5089cf8583351de8814859871d281c004ba8
Philip Schelin <philip.schelin@student.forsbergsskola.se>
2023-10-11 14:23:18 +0200

    Completed P2_1PlaterGold, P2_2ItemValues, and P2Fields

---
+++ P2_1PlayerGold/Program.cs
@@ -0,0 +1,8 @@
+﻿Player player;
+
+player = new Player();
+
+player.gold = 200;
+
+Console.WriteLine(player.gold);
+
---
+++ P2_2ItemValues/Program.cs
@@ -0,0 +1,16 @@
+﻿Item[] items = new Item[3];
+
+items[0] = new Item();
+items[0].goldValue = 100;
+
+items[1] = new Item();
+items[1].goldValue = 200;
+
+items[2] = new Item();
+items[2].goldValue = 300;
+
+for (var i = 0; i < items.Length; i++)
+{
+    Console.WriteLine(items[i].goldValue);
+}
+
